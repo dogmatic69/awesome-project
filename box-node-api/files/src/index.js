078a083c15ba8bec16ea5c78b61cb0220b6b9a9f
Carl Sutton <carl.sutton@ikea.com>
2019-12-14 23:32:39 +0100

    chore!: rename services

--- box-node-api/files/src/index.js
+++ box-node-api/files/src/index.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
   res.send({
-    server: 'api-1',
+    server: 'node-api',
     message: 'Hello, world!',
     error: false,
   });
